ed8e8129da142f51719124c48ef7bb76550a7e63
SirBrado <lewisindustries@gmail.com>
2013-10-10 12:49:46 -0400

    First upload

---
+++ Generic Mod/genericmod_common/methtic/genericmod/GenericMod.java
@@ -0,0 +1,29 @@
+package methtic.genericmod;
+
+import methtic.genericmod.lib.Reference;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.Init;
+import cpw.mods.fml.common.Mod.PostInit;
+import cpw.mods.fml.common.Mod.PreInit;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod( modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+
+public class GenericMod {
+	
+	@PreInit
+	public void preInit(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@Init
+	public void init(FMLPreInitializationEvent event) {
+		
+	}
+	
+	@PostInit
+	public void postInit(FMLPreInitializationEvent event) {
+		
+	}
+
+}
---
+++ Generic Mod/genericmod_common/methtic/genericmod/lib/Reference.java
@@ -0,0 +1,9 @@
+package methtic.genericmod.lib;
+
+public class Reference {
+	
+	public static final String MOD_ID = "GM";
+	public static final String MOD_NAME = "Generic Mod";
+	public static final String VERSION = "0.0.1";
+
+}
